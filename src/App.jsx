38c586381d8dedad25dd59bac3859f2bd53ccb96
manthan <manthankhamkar9@gmail.com>
2024-08-29 21:09:11 +0530

    added ecom

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Collection from './pages/Collection'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vm] md:px-[7vm] lg:px-[9vm] '>
+    <h1> Hii Mihir here 
+
+    </h1>
+
+      <Navbar />
+
+      <Routes>
+        < Route path='/' element={<Home />} />
+        < Route path='/collection' element={<Collection/>}  />
+        < Route path='/product/:productId' element={<Product/>}  />
+        < Route path='/cart' element={<Cart/>}  />
+        < Route path='/place-order' element={<PlaceOrder/>}  />
+        < Route path='/orders' element={<Orders/>}  />
+        < Route path='/about' element={<About/>}  />
+        < Route path='/contact' element={<Contact/>}  />
+        < Route path='/login' element={<Login/>}  />
+      </Routes>
+
+
+    </div>
+  )
+}
+
+export default App
